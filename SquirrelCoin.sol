bf4b5ac1e74f517e554608615628aa2306e85466
Kevin Jin <62975903+kevjin02@users.noreply.github.com>
2022-05-22 23:19:26 -0500

    Update SquirrelCoin.sol

--- SquirrelCoin.sol
+++ SquirrelCoin.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) public ERC20("SquirrelCoin", "SQC") {
+    constructor(uint256 initialSupply) public ERC20("SquirrelCoin", "SQC") { // Replace SquirrelCoin and SQC with your desired token name and abbreviation
         _mint(msg.sender, initialSupply);
     }
 }
